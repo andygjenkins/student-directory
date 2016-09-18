5241731ecff68efb355d26284c37398959074781
andy jenkins <andygjenkins@gmail.com>
2016-09-18 19:18:24 +0100

    Add default load of file

--- directory.rb
+++ directory.rb
@@ -154,8 +154,9 @@ end
 
 def try_load_students
   filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
+  if filename.nil?  # get out of the method if it isn't given
+    load_students
+  elsif File.exists?(filename) # if it exists
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else
@@ -168,5 +169,5 @@ def add_student(name, cohort, cob)
   @students << {name: name, cohort: cohort.to_sym, cob: cob}
 end
 
-
+try_load_students
 interactive_menu
